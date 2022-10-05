80c5bd947c0da5add07b070585d7283710d12184
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-10-05 20:43:00 +0300

    Updates

---
+++ precache-manifest.03c1aa381a70a28aaf1f44f81d8f145d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e18b8bdc3bb2967c8dbe6dbc0de016d",
+    "url": "/HereIsLatestVersianOfTodolistApp/index.html"
+  },
+  {
+    "revision": "3cbd482db477d42571c9",
+    "url": "/HereIsLatestVersianOfTodolistApp/static/css/main.3b2e0d17.chunk.css"
+  },
+  {
+    "revision": "3c302df2961527da8e6d",
+    "url": "/HereIsLatestVersianOfTodolistApp/static/js/2.00e1dc51.chunk.js"
+  },
+  {
+    "revision": "0ca2dd5f1fc5d551b1a81930def3366f",
+    "url": "/HereIsLatestVersianOfTodolistApp/static/js/2.00e1dc51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbd482db477d42571c9",
+    "url": "/HereIsLatestVersianOfTodolistApp/static/js/main.d4ba4781.chunk.js"
+  },
+  {
+    "revision": "4deb0df8e51717156ca1",
+    "url": "/HereIsLatestVersianOfTodolistApp/static/js/runtime-main.c2e7957f.js"
+  }
+]);
